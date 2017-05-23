b5867b8bd9e21ba0b6985c2d538096285b630dc8
haykam821 <haykam821@users.noreply.github.com>
2017-05-22 23:39:16 -0400

    Upload from first working update

---
+++ Reload.obp.js
@@ -0,0 +1,18 @@
+exports.onMessageReceived = (function Reload(bot, doc, user, userID, channelID, message, messageID, event) {
+    require('../Exports.js').registerCmd('reload','Reloads the plugins placed in the plugins folder.');
+    if(message === undefined){
+        return;
+    }
+        if (message === doc.prefix + "reload") {
+            bot.sendMessage({
+            to: doc.logchannel,
+            message: "<@" + userID + "> asked to reload the plugins."
+        });
+        bot.sendMessage({
+            to: channelID,
+            message: "The plugins have been reloaded."
+        });
+        require('../Onebot.js').loadPlugins();
+    }
+
+});
